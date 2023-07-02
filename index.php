ceb40d2a7c7c7146c450329eafa78161236d0e36
Tony Sokolik <asokolik@gmail.com>
2023-07-02 12:59:05 -0400

    Add support for multiple servers to be specified (up to 10). Will support the following environment variables:   - MINECRAFT_SERVER   - MINECRAFT_SERVER1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ html {
 }
 /* Bottom right text */
 .text {
-  position: relative;
+  position: absolute;
   bottom: 20px;
   right: 20px;
   background-color: black;
@@ -23,36 +23,49 @@ html {
 
 <?php
 
-echo "<html><head><title>Status for " . $_ENV["MINECRAFT_SERVER"] . "</title>";
-echo '<link rel="icon" type="image/x-icon" href="/img/favicon.ico">';
-echo '<body><div class="text"><h3>Status for ' . $_ENV["MINECRAFT_SERVER"] . '</h3><p>';
+function show_info($server) {
+  echo "<html><head><title>Status for " . $server . "</title>";
+  echo '<link rel="icon" type="image/x-icon" href="/img/favicon.ico">';
+  echo '<body><div class="text"><h3>Status for ' . $server . '</h3><p>';
 
-$url = "https://api.mcsrvstat.us/bedrock/2/" . $_ENV["MINECRAFT_SERVER"];
-$results = file_get_contents($url);
-$status = json_decode($results);
+  $url = "https://api.mcsrvstat.us/bedrock/2/" . $server;
+  $results = file_get_contents($url);
+  $status = json_decode($results);
 
-if (ip2long($_ENV["MINECRAFT_SERVER"])) {
-	$ipAddress = $_ENV["MINECRAFT_SERVER"];
-  $hostname = gethostbyaddr($_ENV["MINECRAFT_SERVER"]);
-} else {
-	$ipAddress = gethostbyname($_ENV["MINECRAFT_SERVER"]);
-  $hostname = $_ENV["MINECRAFT_SERVER"];
-}
+  if (ip2long($server)) {
+	  $ipAddress = $server;
+    $hostname = gethostbyaddr($server);
+  } else {
+	  $ipAddress = gethostbyname($server);
+    $hostname = $server;
+  }
 
   if ($status->online) {
-  echo "MOTD          : " . $status->motd->html[0] . "<br>\n";
-  echo "IP            : " . $ipAddress . "<br>\n";
-  echo "Hostname      : " . $hostname . "<br>\n";
-  echo "Port          : " . $status->port . "<br>\n";
-  echo "Version       : " . $status->version . "<br>\n";
-  echo "Protocol      : " . $status->protocol . "<br>\n";
-  echo "Server ID     : " . $status->serverid . "<br>\n";
-  echo "Game Mode     : " . $status->gamemode . "<br>\n";
-  echo "Map           : " . $status->map . "<br>\n";
-  echo "Players Online: " . $status->players->online . "<br>\n";
-  echo "Max Players   : " . $status->players->max . "<br>\n";
-} else {
-  echo "Server " . $hostname . " at " . $ipAddress . " is currently offline!\n";
+    echo "MOTD          : " . $status->motd->html[0] . "<br>\n";
+    echo "IP            : " . $ipAddress . "<br>\n";
+    echo "Hostname      : " . $hostname . "<br>\n";
+    echo "Port          : " . $status->port . "<br>\n";
+    echo "Version       : " . $status->version . "<br>\n";
+    echo "Protocol      : " . $status->protocol . "<br>\n";
+    echo "Server ID     : " . $status->serverid . "<br>\n";
+    echo "Game Mode     : " . $status->gamemode . "<br>\n";
+    echo "Map           : " . $status->map . "<br>\n";
+    echo "Players Online: " . $status->players->online . "<br>\n";
+    echo "Max Players   : " . $status->players->max . "<br>\n";
+  } else {
+    echo "Server " . $hostname . " at " . $ipAddress . " is currently offline!\n";
+  }
+
+}
+
+if ($_ENV["MINECRAFT_SERVER"]) {
+  show_info($_ENV["MINECRAFT_SERVER"]);
+}
+
+$maxServer = 10;
+
+for ($i = 1; $i <= $maxServers; $i++) {
+  show_info($_ENV["MINECRAFT_SERVER" . $i]);
 }
 
 echo "</p></div></body></html>";
